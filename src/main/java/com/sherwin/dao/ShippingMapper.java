68b8db4464f85d2e9d8b82c384725e90c252efbd
世文 <sherwindeng@shiwendeMacBook-Pro.local>
2018-04-26 16:43:24 +0800

    init project

---
+++ src/main/java/com/sherwin/dao/ShippingMapper.java
@@ -0,0 +1,17 @@
+package com.sherwin.dao;
+
+import com.sherwin.pojo.Shipping;
+
+public interface ShippingMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Shipping record);
+
+    int insertSelective(Shipping record);
+
+    Shipping selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Shipping record);
+
+    int updateByPrimaryKey(Shipping record);
+}
